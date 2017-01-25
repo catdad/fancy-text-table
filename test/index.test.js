b6a706242451e9c1cecb84b6c762ba353986c4fa
Kiril Vatev <vatev.1@gmail.com>
2017-01-24 21:35:52 -0500

    adding some tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,30 @@
 
 var expect = require('chai').expect;
 
+var table = require('../');
+
 describe('[index]', function () {
   describe('#row', function () {
-    it('takes an array');
+    it('takes an array and renders items as individual cells', function () {
+      var tbl = table();
 
-    it('renders items as individual cells');
+      tbl.row([1, 2, 3]);
+
+      var str = tbl.render();
+
+      expect(str).to.match(/^1\s{2}2\s{2}3$/);
+    });
+
+    it('aligns multiple rows', function () {
+      var tbl = table();
+
+      tbl.row([11, 22, 33]);
+      tbl.row([1, 2, 3]);
+
+      var str = tbl.render();
+
+      expect(str).to.match(/^11\s{2}22\s{2}33\n1\s{3}2\s{3}3$/);
+    });
   });
 
   describe('#title', function () {
