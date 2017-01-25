540ec4882a7c74b912940eb85dbd1ebc643cf4eb
Kiril Vatev <vatev.1@gmail.com>
2017-01-24 21:44:13 -0500

    chaning all the code to two spaces

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ function includes(arr, item) {
   return arr.indexOf(item) >= 0;
 }
 
-module.exports = function() {
-    var titles = [];
-    var rows = [];
+module.exports = function () {
+  var titles = [];
+  var rows = [];
 
-    return {
-        row: function(values) {
-            rows.push(values);
-        },
-        title: function(value) {
-            titles.push(value);
-            rows.push([value]);
-        },
-        line: function() {
-            rows.push(['']);
-        },
-        render: function() {
-            return table(rows, {
-                stringLength: function(str) {
-                    if (includes(titles, str)) {
-                        return 0;
-                    }
+  return {
+    row: function (values) {
+      rows.push(values);
+    },
+    title: function (value) {
+      titles.push(value);
+      rows.push([value]);
+    },
+    line: function () {
+      rows.push(['']);
+    },
+    render: function () {
+      return table(rows, {
+        stringLength: function (str) {
+          if (includes(titles, str)) {
+            return 0;
+          }
 
-                    return unstyle.string(str).length;
-                },
-              hsep: '  '
-            });
-        }
-    };
+          return unstyle.string(str).length;
+        },
+        hsep: '  '
+      });
+    }
+  };
 };
