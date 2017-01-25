4c10906974037a78025dae7c0bddc1f520e8d809
Kiril Vatev <vatev.1@gmail.com>
2017-01-24 21:27:03 -0500

    adding test stubs

--- test/index.test.js
+++ test/index.test.js
@@ -3,5 +3,23 @@
 var expect = require('chai').expect;
 
 describe('[index]', function () {
+  describe('#row', function () {
+    it('takes an array');
 
+    it('renders items as individual cells');
+  });
+
+  describe('#title', function () {
+    it('takes a string');
+
+    it('renders the title as a single cell spanning the entire table');
+  });
+
+  describe('#line', function () {
+    it('renders as a break in the table');
+  });
+
+  describe('#render', function () {
+    it('returns a string');
+  });
 });
