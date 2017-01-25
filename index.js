9c25476d3ba840a73803b04e4c1fd6eb6fd6ddf3
Kiril Vatev <vatev.1@gmail.com>
2017-01-24 21:22:41 -0500

    adding initial implementation

---
+++ index.js
@@ -0,0 +1,37 @@
+/* jshint node: true */
+
+var table = require('text-table');
+var unstyle = require('unstyle');
+
+function includes(arr, item) {
+  return arr.indexOf(item) >= 0;
+}
+
+module.exports = function() {
+    var titles = [];
+    var rows = [];
+
+    return {
+        row: function(values) {
+            rows.push(values);
+        },
+        title: function(value) {
+            titles.push(value);
+            rows.push([value]);
+        },
+        line: function() {
+            rows.push(['']);
+        },
+        render: function() {
+            return table(rows, {
+                stringLength: function(str) {
+                    if (includes(titles, str)) {
+                        return 0;
+                    }
+
+                    return unstyle.string(str).length;
+                }
+            });
+        }
+    };
+};
